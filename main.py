32c576530c536b09903c59920cd6ba8b4708e387
maxjeschek@bstreet.de <maxjeschek@bstreet.de>
2023-07-04 10:30:23 +0200

    update sorting

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from os import listdir, getcwd
 from os.path import isfile, join, isdir, getsize
 
-def get_size(file_path):
-  file_size = getsize(file_path)
-  unit = 'bytes'
+def get_size(file_size):
+  unit = 'Bytes'
 
   if file_size > 1024:
-    unit = 'kb'
+    unit = 'KB'
   elif file_size > 1024 ** 2:
-    unit = 'mb'
+    unit = 'MB'
   elif file_size > 1024 ** 3:
-    unit = 'gb'
+    unit = 'GB'
 
-  exponents_map = {'bytes': 0, 'kb': 1, 'mb': 2, 'gb': 3}
+  exponents_map = {'Bytes': 0, 'KB': 1, 'MB': 2, 'GB': 3}
   size = file_size / 1024 ** exponents_map[unit]
 
-  if unit == 'bytes':
+  if unit == 'Bytes':
     return str(file_size) + " " + unit
   else:
     return round(size, 3).__str__() + " " + unit
@@ -26,13 +25,13 @@ def get_files_of_folder(path):
     
     for file in files:
       if isfile(join(path, file)):
-        returning_files.append((file, join(path, file), get_size(join(path, file))))
-      elif isdir(join(path, file)):
+        size = getsize(join(path, file))
+        size_description = get_size(size)
+        returning_files.append((file, size, size_description))
+      if isdir(join(path, file)):
         appending_files = get_files_of_folder(join(path, file))
         for appending_file in appending_files:
           returning_files.append(appending_file)
-      else:
-        print("Error: " + file + " is not a file or directory")
 
     return returning_files
         
@@ -44,7 +43,7 @@ if __name__ == '__main__':
     # get the files of the current working directory
     files = get_files_of_folder(cwd)
     # sort files by size
-    files.sort(key=lambda x: x[2], reverse=True)
+    files.sort(key=lambda x: x[1], reverse=True)
 
     print("Filename".ljust(50, " ") + "Size")
     print("-" * 60)
@@ -52,11 +51,23 @@ if __name__ == '__main__':
     if len(files) <= 10:
       #print the files with their size
       for file in files:
+        filename = file[0]
+        if len(filename) > 50:
+          # take the last 50 characters of the file name
+          filename = file[0][-50:]
+        filename = file[0].ljust(50, " ")
+
         # print the file name and the size with the name as a fixed length
-        print(file[0].ljust(50, " ") +  file[2])
+        print(filename +  file[2])
     else:
       #print the 10 biggest files with their size
       for i in range(10):
+        filename = files[i][0]
+        if len(filename) > 50:
+          # take the last 50 characters of the file name
+          filename = files[i][0][-50:]
+        filename = files[i][0].ljust(50, " ")
+
         # print the file name and the size with the name as a fixed length
-        print(files[i][0].ljust(50, " ") + files[i][2])
+        print(filename + files[i][2])
     
